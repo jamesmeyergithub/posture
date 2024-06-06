9dde449cf5745a081e50d89cdd93c84d6e116569
James Meyer <jamespmeyer@yahoo.com>
2024-06-06 23:30:58 +0000

    Updating

--- acme.tf
+++ acme.tf
@@ -1,11 +1,43 @@
-provider "google" {
-  project = "iamdemok8"
-  region  = "us-east1"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
 }
 
-resource "google_storage_bucket" "acme-bucket-a66" {
-  name          = "acme-bucket-a66"
+provider "google" {
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "example_network"{
+  name                            = "example-network-1"
+  delete_default_routes_on_create = false
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  mtu                             = 100
+  project                         = "iamdemok8"
+}
+
+resource "google_container_node_pool" "example_node_pool" {
+  name               = "example-node-pool-1"
+  cluster            = "example-cluster-1"
+  project            = "iamdemok8"
+  initial_node_count = 2
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+  }
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name          = "example-bucket-1"
   location      = "EU"
   force_destroy = true
-  uniform_bucket_level_access = true
+
+  project = "iamdemok8"
+
+  uniform_bucket_level_access = false
 }
