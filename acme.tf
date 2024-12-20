b187e1bd4585e348adf220b5740ad07b39d0e6c3
James Meyer <jamespmeyer@yahoo.com>
2024-12-20 18:53:30 +0000

    adding bad vm

--- acme.tf
+++ acme.tf
@@ -12,7 +12,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "example_network"{
-  name                            = "example-network-1"
+  name                            = "default"
   delete_default_routes_on_create = false
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
@@ -20,17 +20,64 @@ resource "google_compute_network" "example_network"{
   project                         = "iamdemok8"
 }
 
-resource "google_container_node_pool" "example_node_pool" {
-  name               = "example-node-pool-1"
-  cluster            = "example-cluster-1"
-  project            = "iamdemok8"
-  initial_node_count = 1
+# resource "google_service_account" "default" {
+#   account_id   = "my-custom-sa"
+#   display_name = "Custom SA for VM Instance"
+# }
 
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
   }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  # service_account {
+  #   # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+  #   email  = google_service_account.default.email
+  #   scopes = ["cloud-platform"]
+  # }
 }
+
+# resource "google_container_node_pool" "example_node_pool" {
+#   name               = "example-node-pool-1"
+#   cluster            = "example-cluster-1"
+#   project            = "iamdemok8"
+#   initial_node_count = 1
+
+#   node_config {
+#     preemptible  = true
+#     machine_type = "e2-medium"
+#   }
+# }
 
 resource "google_storage_bucket" "example_bucket" {
   name          = "example-bucket-1"
