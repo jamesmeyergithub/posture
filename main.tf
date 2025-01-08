a61785ad17f67b511e0061bf214a72c9f1ccb9c1
James Meyer <jamespmeyer@yahoo.com>
2025-01-08 21:46:31 +0000

    update bq to public

--- main.tf
+++ main.tf
@@ -111,3 +111,16 @@ resource "google_storage_bucket" "example_bucket" {
 
   uniform_bucket_level_access = true
 }
+
+resource "google_bigquery_dataset" "public" {
+  dataset_id                  = "public"
+  friendly_name               = "test"
+  description                 = "This dataset is public"
+ # location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  access {
+    role   = "READER"
+    iam_member =  "allUsers"
+  }
+}
